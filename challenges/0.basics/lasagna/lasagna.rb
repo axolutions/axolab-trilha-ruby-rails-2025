96395114b1f27f6893f6438a6be371f875ed7381
muri <murichristopher@outlook.com>
2025-01-20 17:36:54 -0300

    update challenge

--- challenges/0.basics/lasagna/lasagna.rb
+++ challenges/0.basics/lasagna/lasagna.rb
@@ -1,13 +1,13 @@
-module Blackjack
-  def self.parse_card(card)
-    raise "Please implement the Blackjack.parse_card method"
+class Lasagna
+  def remaining_minutes_in_oven(actual_minutes_in_oven)
+    raise 'Please implement the Lasagna#remaining_minutes_in_oven method'
   end
 
-  def self.card_range(card1, card2)
-    raise "Please implement the Blackjack.card_range method"
+  def preparation_time_in_minutes(layers)
+    raise 'Please implement the Lasagna#preparation_time_in_minutes method'
   end
 
-  def self.first_turn(card1, card2, dealer_card)
-    raise "Please implement the Blackjack.first_turn method"
+  def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
+    raise 'Please implement the Lasagna#total_time_in_minutes method'
   end
 end
